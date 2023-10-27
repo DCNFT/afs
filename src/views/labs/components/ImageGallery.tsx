6dedccc5b98dc8159b4163db1c9b0e2b0081b011
westkite1201 <westkite1201@gmail.com>
2023-10-27 19:02:17 +0900

    feat : masonry 라이브러리 추가

--- src/views/labs/components/ImageGallery.tsx
+++ src/views/labs/components/ImageGallery.tsx
@@ -2,34 +2,50 @@ import React from 'react';
 import { useQuery } from '@tanstack/react-query';
 import { fetchImages } from '@/api/unsplash';
 import { Image } from '@/api/unsplash/types';
-import {
-  MasonryGrid,
-  JustifiedGrid,
-  FrameGrid,
-  PackingGrid,
-} from '@egjs/react-grid';
+import Masonry from 'react-masonry-component';
 
+const masonryOptions = {
+  transitionDuration: 0,
+};
+
+const imagesLoadedOptions = { background: '.my-bg-image-el' };
+
+// <MasonryGrid
+//   className="container"
+//   style={{ overflow: 'hidden' }}
+//   gap={5}
+//   defaultDirection={'end'}
+//   align={'justify'}
+// >
+//   {images?.map((image: Image) => (
+//     <div key={image.id} className="item">
+//       <img
+//         src={image.urls.small}
+//         alt={image.alt_description || 'Unsplash Image'}
+//         className="absolute w-full h-full object-cover"
+//       />
+//     </div>
+//   ))}
+// </MasonryGrid>
 const ImageGallery = () => {
   const { data: images, error, isLoading } = useQuery(['images'], fetchImages); // Use the useImages function
 
   return (
-    <MasonryGrid
-      className="container"
-      style={{ overflow: 'hidden' }}
-      gap={5}
-      defaultDirection={'end'}
-      align={'justify'}
+    <Masonry
+      className={'my-gallery-class'} // default ''
+      elementType={'ul'} // default 'div'
+      options={masonryOptions} // default {}
+      disableImagesLoaded={false} // default false
+      updateOnEachImageLoad={false} // default false and works only if disableImagesLoaded is false
+      imagesLoadedOptions={imagesLoadedOptions} // default {}
     >
-      {images?.map((image: Image) => (
-        <div key={image.id} className="my-masonry-grid_column">
-          <img
-            src={image.urls.small}
-            alt={image.alt_description || 'Unsplash Image'}
-            className="absolute w-full h-full object-cover"
-          />
+      <li className="image-element-class">
+        <div>
+          <img src="path/to/image" />
         </div>
-      ))}
-    </MasonryGrid>
+      </li>
+      // more elements
+    </Masonry>
   );
 };
 
