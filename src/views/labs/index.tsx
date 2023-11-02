253b0744aef33e63748a3419d30b466e90c604c2
westkite1201 <westkite1201@gmail.com>
2023-11-02 17:56:53 +0900

    style : layout 스타일 변경

--- src/views/labs/index.tsx
+++ src/views/labs/index.tsx
@@ -11,7 +11,7 @@ const Labs = () => {
   }, []);
 
   return (
-    <div className=" max-w-full h-full max-h-full w-[1440px] flex gap-5xl px-md py-xl desktop:px-[128px] tablet:px-[64px] mobile:px-md">
+    <div className=" max-w-full h-full max-h-full w-full flex gap-5xl px-md py-xl desktop:px-[128px] tablet:px-[64px] mobile:px-md">
       {/* {isClient ? <ImageGallery /> : <div className="h-screen w-[428px]"></div>} */}
       <div className="relative flex w-full items-center justify-center tablet:h-auto">
         <ImageUploader />
